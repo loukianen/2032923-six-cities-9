b5363cfba463f11793abec255fa3b2c4006399ef
Konstantin Lukyanenok <loukianen@gmail.com>
2022-02-12 18:04:07 +0300

    finish module2-task1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,6 +4,6 @@ import App from './components/app/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App advertsAmount={312}/>
   </React.StrictMode>,
   document.getElementById('root'));
