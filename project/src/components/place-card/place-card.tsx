d887042d50ea9a22c40126f8fbdad57da6b6a4de
Konstantin Lukyanenok <loukianen@gmail.com>
2022-02-23 18:11:20 +0300

    modify PlaceCard for receive props

--- project/src/components/place-card/place-card.tsx
+++ project/src/components/place-card/place-card.tsx
@@ -1,18 +1,21 @@
-function PlaceCard(): JSX.Element {
+import { Offer } from '../../types/offers';
+
+function PlaceCard(props: Offer): JSX.Element {
+  const { isPremium, previewImage, price, rating, title, type } = props;
   return (
     <article className="cities__place-card place-card">
-      <div className="place-card__mark">
+      <div className="place-card__mark" hidden={!isPremium}>
         <span>Premium</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#place-card">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place" />
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -24,14 +27,14 @@ function PlaceCard(): JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: `${rating * 10}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#place-card">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#place-card">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
