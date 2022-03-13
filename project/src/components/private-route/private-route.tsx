10d4c21dbcbe4e61906e38cc6592566f89dfe763
Konstantin Lukyanenok <loukianen@gmail.com>
2022-03-13 12:34:01 +0300

    connect PrivateRout to store

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,11 +1,13 @@
 import {Navigate} from 'react-router-dom';
+import {useAppSelector} from '../../hooks/hooks';
 
 type PrivateRouteProps = {
   children: JSX.Element;
 };
 
 function PrivateRoute({children}: PrivateRouteProps): JSX.Element {
-  const hasAccess = true;
+  const { authorizationStatus } = useAppSelector((state) => state);
+  const hasAccess = authorizationStatus === 'authorized';
 
   return hasAccess ? children : <Navigate to={'/login'} />;
 }
