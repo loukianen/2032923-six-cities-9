93fe9590afcc43aacc014f05ffbeb26b70426f27
Konstantin Lukyanenok <loukianen@gmail.com>
2022-03-17 12:44:29 +0300

    optimize PlaceCardList rerendering

--- project/src/components/place-card-list/place-card-list.tsx
+++ project/src/components/place-card-list/place-card-list.tsx
@@ -1,15 +1,16 @@
+import {memo} from 'react';
 import PlaceCard from '../place-card/place-card';
 import {Offer} from '../../types/offers';
 import {PlaceCardListType} from '../../types/other-types';
 
-function getClassName(type: PlaceCardListType ): string {
-  const mapping = {
-    main: 'cities__places-list places__list tabs__content',
-    room: 'near-places__list places__list',
-  };
-  return mapping[type];
-}
+const placeCardClassNameMapping = {
+  main: 'cities__places-list places__list tabs__content',
+  room: 'near-places__list places__list',
+};
 
+function getPlaceCardClassName(type: PlaceCardListType ): string {
+  return placeCardClassNameMapping[type];
+}
 
 type PlaceCardListProps = {
   offers: Offer[],
@@ -20,7 +21,7 @@ type PlaceCardListProps = {
 function PlaceCardList(props: PlaceCardListProps) {
   const { offers, placeCardListType } = props;
   return (
-    <div className={getClassName(placeCardListType)}>
+    <div className={getPlaceCardClassName(placeCardListType)}>
       {offers.map((offer) => (
         <PlaceCard
           key={offer.id}
@@ -33,4 +34,9 @@ function PlaceCardList(props: PlaceCardListProps) {
   );
 }
 
-export default PlaceCardList;
+export default memo(PlaceCardList, (prevProps, nextProps) => {
+  const isOfferIdsEqual = (prevOffers: Offer[], nextOffers: Offer[]) => prevOffers.every(
+    (item, index) => item.id === nextOffers[index].id);
+  return isOfferIdsEqual(prevProps.offers, nextProps.offers)
+    && prevProps.placeCardListType === nextProps.placeCardListType;
+});
