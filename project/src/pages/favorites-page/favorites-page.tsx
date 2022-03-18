bcf1c1f65515635ecc1eab949da2af527220f445
Konstantin Lukyanenok <loukianen@gmail.com>
2022-03-18 11:31:02 +0300

    add FavoritesEmpty

--- project/src/pages/favorites-page/favorites-page.tsx
+++ project/src/pages/favorites-page/favorites-page.tsx
@@ -1,50 +1,28 @@
-import { OffersProps } from '../../types/offers';
+import cn from 'classnames';
+import Header from '../../components/header/header';
 import FavoriteLocationsList from '../../components/favorite-locations-list/favotie-locations-list';
+import FavoritesEmpty from '../../components/favorites-empty/favorites-empty';
+import FavoritesFooter from '../../components/favorites-footer/favorites-footer';
+import { OffersProps } from '../../types/offers';
 
 function FavoritesPage(props: OffersProps): JSX.Element {
+  const isFaviritesEmpty = props.offers.length === 0;
+
+  const mainClassName = cn('page__main', 'page__main--favorites', {
+    'page__main--favorites-empty': isFaviritesEmpty,
+  });
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#header__nav">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#header__nav">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
-      <main className="page__main page__main--favorites">
+      <main className={mainClassName}>
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoriteLocationsList offers={props.offers} />
-          </section>
+          {isFaviritesEmpty
+            ? <FavoritesEmpty />
+            : <FavoriteLocationsList offers={props.offers} />}
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
-      </footer>
+      <FavoritesFooter />
     </div>
   );
 }
