bfe5ff5776e211259919c97a8a3527ff3754bbcb
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-21 12:10:46 +0300

    some demo to solve problems with your solutions

--- project/src/components/bookmark/bookmark.tsx
+++ project/src/components/bookmark/bookmark.tsx
@@ -1,48 +1,56 @@
-import cn from 'classnames';
-import {useAppSelector, useAppDispatch} from '../../hooks/hooks';
-import {changeOfferStatusAction} from '../../store/api-actions';
-import {redirectToRoute} from '../../store/actions';
-import {PlaceCardType} from '../../types/other-types';
-import {AppRoute, NameSpace} from '../../const';
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
+import { changeOfferStatusAction } from '../../store/api-actions';
+import { PlaceCardType } from '../../types/other-types';
+import { AppRoute, NameSpace } from '../../const';
+import { useCallback } from 'react';
+import { Link } from 'react-router-dom';
 
-function Bookmark(props: {hotelId: number, isFavorite: boolean, type: PlaceCardType}): JSX.Element {
-  const dispatch = useAppDispatch();
-  const authStatus = useAppSelector((state) => state[NameSpace.auth]);
-  const {hotelId, isFavorite, type} = props;
-  const isTypeRoom = type === 'room';
+const getWidth = (isRoom: boolean) => isRoom ? '31' : '18';
+const getHeight = (isRoom: boolean) => isRoom ? '33' : '19';
+const getSvgClassName = (isRoom: boolean) => isRoom ? 'property__bookmark-icon' : 'place-card__bookmark-icon';
+const getElementClassName = (isRoom: boolean) => `${isRoom ? 'property__bookmark-button' : 'place-card__bookmark-button'}`;
+const getModifier = (className: string, isFavorite: boolean) => `${className} ${isFavorite ? `${className}--active` : ''}`;
+const getButtonClassName = (isRoom: boolean, isFavorite: boolean) => `button ${getModifier(getElementClassName(isRoom), isFavorite)}`;
 
-  function toggleStatus() {
-    if (authStatus !== 'authorized') {
-      dispatch(redirectToRoute(AppRoute.Login));
-    } else {
-      const newStatus = !isFavorite;
-      dispatch(changeOfferStatusAction(hotelId, newStatus, type));
-    }
-  }
+const getViewProps = (isTypeRoom: boolean, isFavorite: boolean) => ({
+  buttonClassName: getButtonClassName(isTypeRoom, isFavorite),
+  svgClassName: getSvgClassName(isTypeRoom),
+  width: getWidth(isTypeRoom),
+  height: getHeight(isTypeRoom),
+});
 
-  const buttonClassName = cn('button', {
-    'place-card__bookmark-button--active button': isFavorite && !isTypeRoom,
-    'property__bookmark-button--active button': isFavorite && isTypeRoom,
-    'place-card__bookmark-button': !isTypeRoom,
-    'property__bookmark-button': isTypeRoom,
-  });
-
-  const svgClassName = cn({
-    'place-card__bookmark-icon': !isTypeRoom,
-    'property__bookmark-icon': isTypeRoom,
-  });
-
-  const width = cn({'18': !isTypeRoom, '31': isTypeRoom});
-  const height = cn({'19': !isTypeRoom, '33': isTypeRoom});
-
+function BookmarkContent(props: { svgClassName: string, width: string, height: string }) {
   return (
-    <button className={buttonClassName} type="button" onClick={toggleStatus}>
-      <svg className={svgClassName} width={width} height={height}>
+    <>
+      <svg className={props.svgClassName} width={props.width} height={props.height}>
         <use xlinkHref="#icon-bookmark"></use>
       </svg>
       <span className="visually-hidden">In bookmarks</span>
-    </button>
+    </>
   );
+}
+
+function Bookmark(props: { hotelId: number, isFavorite: boolean, type: PlaceCardType }): JSX.Element {
+  const dispatch = useAppDispatch();
+  const authStatus = useAppSelector((state) => state[NameSpace.auth]);
+  const { hotelId, isFavorite, type } = props;
+  const {
+    buttonClassName,
+    height,
+    svgClassName,
+    width,
+  } = getViewProps(type === 'room', isFavorite);
+
+  const toggleStatus = useCallback(() => {
+    dispatch(changeOfferStatusAction(hotelId, !isFavorite, type));
+  }, [hotelId, isFavorite, type, dispatch]);
+
+
+  const content = <BookmarkContent height={height} svgClassName={svgClassName} width={width} />;
+  return authStatus === 'authorized' ?
+    (<button className={buttonClassName} type="button" onClick={toggleStatus}>{content}</button>) :
+    (<Link className={buttonClassName} to={AppRoute.Login}>{content}</Link>);
+
 }
 
 export default Bookmark;
