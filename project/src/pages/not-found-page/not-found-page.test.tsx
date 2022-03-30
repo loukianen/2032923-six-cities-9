e68f82ff2e3a97b944e2bf73ce5f0d413e505af4
Konstantin Lukyanenok <loukianen@gmail.com>
2022-03-30 20:28:07 +0300

    modify NotFoundPage test

--- project/src/pages/not-found-page/not-found-page.test.tsx
+++ project/src/pages/not-found-page/not-found-page.test.tsx
@@ -1,12 +1,48 @@
+import {Routes, Route} from 'react-router-dom';
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {createMemoryHistory, History} from 'history';
+import HistoryRouter from '../../components/history-router/history-router';
 import NotFoundPage from './not-found-page';
+import {AppRoute} from '../../const';
+
+const renderNotFoundPage = (history: History) => {
+  render(
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<h1>Main page</h1>}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </HistoryRouter>,
+  );
+};
+
+const history = createMemoryHistory();
 
 describe('Component: NotFoundPage', () => {
+  beforeEach(() => {
+    history.push(AppRoute.NotFound);
+  });
+
   it('should render correctly', () => {
-    render(<NotFoundPage />);
+    renderNotFoundPage(history);
 
     expect(screen.getByText(/404. Page not found/i)).toBeInTheDocument();
-    expect(screen.getByText(/Вернуться на главную/i)).toBeInTheDocument();
-    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByText('Come back to main page')).toBeInTheDocument();
+  });
+
+  it('should navigate to MainPage if user click link', () => {
+    renderNotFoundPage(history);
+
+    userEvent.click(screen.getByRole('link'));
+
+    expect(history.location.pathname).toBe(AppRoute.Root);
+    expect(screen.getByText('Main page')).toBeInTheDocument();
   });
 });
