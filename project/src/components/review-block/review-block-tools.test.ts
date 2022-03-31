f64856dc04f6edabaa5b02fe6b30fbd27d8191d8
Konstantin Lukyanenok <loukianen@gmail.com>
2022-03-31 15:15:36 +0300

    fix comments amount rendering

---
+++ project/src/components/review-block/review-block-tools.test.ts
@@ -0,0 +1,18 @@
+import makeFakeComments from '../../mocks/comments';
+import {getCommentsForRendering} from './review-block';
+import {shufleArray} from '../../services/utils';
+import {Comment} from '../../types/offers';
+import {REVIEW} from '../../const';
+
+const commentCount = REVIEW.MaxCount + 2;
+const datesForFakeComments = Array(commentCount).fill(31).map((el, i) => `2022-03-${el - i}`);
+
+const fakeComments = makeFakeComments(commentCount);
+const commentsWithCustomDate = fakeComments.map((item, i) => ({...item, date: datesForFakeComments[i]}));
+
+const controlComments = commentsWithCustomDate.slice(0, REVIEW.MaxCount);
+const testData = shufleArray(commentsWithCustomDate) as Comment[];
+
+it('should resultComments be equal to control comments', () =>{
+  expect(getCommentsForRendering(testData)).toEqual(controlComments);
+});
--- project/src/services/utils.ts
+++ project/src/services/utils.ts
@@ -25,6 +25,19 @@ export function getRatingStyleData(rating: number) {
   return Math.round(rating) * 20;
 }
 
+export function shufleArray(arr: Array<unknown>) {
+  function iter(currentArr: Array<unknown>, result: Array<unknown>) : Array<unknown>{
+    if (currentArr.length === 0) {
+      return result;
+    }
+    const randomIndex = getRandomArrayIndex(currentArr);
+    const newResult = [...result, currentArr[randomIndex]];
+    const rest = [...currentArr.slice(0, randomIndex), ...currentArr.slice(randomIndex + 1)];
+    return iter(rest, newResult);
+  }
+  return iter(arr, []);
+}
+
 export const uniqueId = Object.assign(
   (prefix = '') => {
     uniqueId.counter += 1;
